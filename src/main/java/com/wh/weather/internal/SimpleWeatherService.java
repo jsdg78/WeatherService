b474a5ffc53e1ba2fe5f471cacfdcfe0377f400d
Jay de Guzman <jsdg@hotmail.com>
2018-03-19 00:06:21 -0400

    Code refactoring and added enhancements

--- src/main/java/com/wh/weather/internal/SimpleWeatherService.java
+++ src/main/java/com/wh/weather/internal/SimpleWeatherService.java
@@ -1,5 +1,7 @@
 package com.wh.weather.internal;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.wh.weather.WeatherClient;
@@ -10,12 +12,16 @@ import com.wh.weather.model.Wind;
 @Service
 public class SimpleWeatherService implements WeatherService {
 
+  private static final Logger LOG = LoggerFactory.getLogger(SimpleWeatherService.class);
+
   @Autowired
   private WeatherClient weatherClient;
 
   @Override
   public Wind getWind(String zipCode) {
     Weather weather = weatherClient.getWeather(zipCode);
+    Wind wind = weather.getWind();
+    LOG.info("Current wind for {}: {}", zipCode, wind);
     return weather.getWind();
   }
 }
